5aadce87ab7f1826428576a6a15472537e8fa5bc
Phat Nguyen <ndtpptdn798789@gmail.com>
2026-08-06 13:32:41 +0700

    fix: delete export removed icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -3,6 +3,3 @@ export { default as LinkIcon } from "./Link";
 export { default as Github } from "./Github";
 export { default as LinkedIn } from "./LinkedIn";
 export { default as Gmail } from "./Gmail";
-export { default as File } from "./File";
-export { default as ChevronLeft } from "./ChevronLeft";
-export { default as ChevronRight } from "./ChevronRight";
