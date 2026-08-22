b078cb17d40dd216bb96fa7424c1b860c13b2f60
Phat Nguyen <ndtpptdn798789@gmail.com>
2026-08-23 01:18:39 +0700

    [v2-fix]: fix build failed

--- components/Separate.tsx
+++ components/Separate.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 
-export default function Separate({ className }: { className: string }) {
+export default function Separate({ className }: { className?: string }) {
   return (
     <div
       className={cn("w-full border-b border-dashed border-border", className)}
